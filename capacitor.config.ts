58a3c6cd831751d3f25e27306a2ee6498c78b828
Abhishek Shrivastava <abhishekkumardipu@gmail.com>
2026-05-15 03:39:14 +0530

    Update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,74 +1,9 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.nextgrid.app",
-
+  appId: "com.nextgridstyle.app",
   appName: "NextGrid Style",
-
   webDir: "out",
-
-  server: {
-    androidScheme: "https",
-  },
-
-  android: {
-    backgroundColor: "#000000",
-
-    allowMixedContent: true,
-
-    captureInput: true,
-  },
-
-  ios: {
-    contentInset: "automatic",
-
-    backgroundColor: "#000000",
-  },
-
-  plugins: {
-    SplashScreen: {
-
-      // Native splash duration
-      launchShowDuration: 2500,
-
-      // Auto hide splash
-      launchAutoHide: true,
-
-      // Disable spinner
-      showSpinner: false,
-
-      // Smooth fade
-      fadeSplashScreen: true,
-
-      fadeSplashScreenDuration: 500,
-
-      // Background
-      backgroundColor: "#000000",
-
-      // Android scaling
-      androidScaleType: "CENTER_CROP",
-
-      // Fullscreen splash
-      splashFullScreen: true,
-
-      // Immersive mode
-      splashImmersive: true,
-    },
-
-    StatusBar: {
-      style: "DARK",
-
-      backgroundColor: "#000000",
-
-      overlaysWebView: false,
-    },
-
-    Keyboard: {
-      resize: "body",
-
-      resizeOnFullScreen: true,
-    },
-  },
 };
 
 export default config;
--- next.config.ts
+++ next.config.ts
@@ -1,46 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-const withPWA = require(
-  "next-pwa"
-)({
-  dest: "public",
-
-  register: true,
-
-  skipWaiting: true,
-
-  disable:
-    process.env.NODE_ENV ===
-    "development",
-});
-
 const nextConfig = {
   output: "export",
-
-  reactStrictMode: true,
-
-  trailingSlash: true,
-
-  poweredByHeader: false,
-
-  compress: true,
-
-  turbopack: {},
-
   images: {
     unoptimized: true,
   },
-
-  experimental: {
-    scrollRestoration: true,
-  },
-
-  compiler: {
-    removeConsole:
-      process.env.NODE_ENV ===
-      "production",
-  },
 };
 
-module.exports =
-  withPWA(nextConfig);
+module.exports = nextConfig;
